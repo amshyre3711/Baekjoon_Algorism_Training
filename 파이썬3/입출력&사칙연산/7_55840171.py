9f1158d5d8f653fec3612d6c276e74ec14173c54
개발자 강건우 <75293565+amshyre3711@users.noreply.github.com>
2023-02-21 20:51:25 +0900

    Add files via upload

---
+++ 파이썬3/입출력&사칙연산/7_55840171.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#https://www.acmicpc.net/source/55840171
+print(input()+"??!")
+
---
+++ 파이썬3/입출력&사칙연산/8_55840485.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#https://www.acmicpc.net/source/55840485
+print(int(input())-543)
+
